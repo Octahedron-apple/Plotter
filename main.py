f5cfce2478697e7e94d929b387fe1b96c5206b81
Octahedron-apple <bhavya1singh1@gmail.com>
2026-01-08 20:41:08 +0530

    Implement menu for equation forms selection
    Added a menu for selecting different forms of equations.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import matplotlib.pyplot as plt
+while True:
+    print("\n1. Slope-Intercept Form")
+    print("2. Intercept Form")
+    print("3. Normal Form")
+    print("4. Exit")
+    
+    ch = input("Select option: ")
+    
+    if ch == '4':
+        break
